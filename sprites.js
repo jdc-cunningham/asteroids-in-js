1cf7155455830f78a9df390795f4f84d32410721
Jacob Cunningham <jcun4128@gmail.com>
2023-09-25 22:33:34 -0500

    programmatize that shi

--- sprites.js
+++ sprites.js
@@ -2,10 +2,21 @@
 // https://www.html5canvastutorials.com/tutorials/html5-canvas-line-color/
 // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineWidth
 // https://stackoverflow.com/questions/8696631/canvas-drawings-like-lines-are-blurry
+// https://stackoverflow.com/questions/2142535/how-to-clear-the-canvas-for-redrawing
 
-const ship = [
-
-];
+const ship = {
+  vertices: [
+    [25, 0],
+    [50, 50],
+    [0, 50],
+    [25, 0],
+  ],
+  center: [12.5, 25],
+  rotation: 0, // deg,
+  position: [12.5, 25],
+  color: '#000000',
+  lineWidth: 2,
+}
 
 // tmp
 const canvas = document.getElementById('canvas');
@@ -18,14 +29,29 @@ canvas.width = pageWidth;
 canvas.height = pageHeight;
 
 var ctx = canvas.getContext('2d');
-// ctx.fillStyle = '#f00';
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(100,50);
-ctx.lineTo(50, 100);
-ctx.lineTo(0, 90);
-ctx.closePath();
-ctx.lineWidth = 2;
-// ctx.fill();
-ctx.strokeStyle = "#000000";
-ctx.stroke();
+
+const clearCanvas = (ctx) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+const draw = (ctx, item) => {
+  // ctx.fillStyle = '#f00';
+  ctx.beginPath();
+
+  item.vertices.forEach((point, index) => {
+    if (index === 0) {
+      ctx.moveTo(point[0], point[1]); // could do obj x, y
+    } else {
+      ctx.lineTo(point[0], point[1]);
+    }
+  });
+
+  ctx.closePath();
+  ctx.lineWidth = item.lineWidth;
+  // ctx.fill();
+  ctx.strokeStyle = item.color;
+  ctx.stroke();
+}
+
+clearCanvas(ctx);
+draw(ctx, ship);

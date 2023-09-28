6e01043014840644ba92050ecdfc10e7d4ac943d
Jacob Cunningham <jcun4128@gmail.com>
2023-09-27 22:24:49 -0500

    bad first attempt vert rotation

--- controls.js
+++ controls.js
@@ -49,6 +49,12 @@ canvas.addEventListener('mousemove', (e) => {
   let degBase = 0;
   let ratio = 0;
 
+  // quadrant as in
+  // 4 | 1
+  // 3 | 2
+
+  let quadrant = 1;
+
   if (mouseCoord[0] >= shipCenter[0] && mouseCoord[1] <= shipCenter[1]) {
     degBase = 0;
 
@@ -56,6 +62,8 @@ canvas.addEventListener('mousemove', (e) => {
       Math.abs(mouseCoord[0] - shipCenter[0]) /
       Math.abs(mouseCoord[1] - shipCenter[1])
     );
+
+    quadrant = 1;
   }
 
   else if (mouseCoord[0] >= shipCenter[0] && mouseCoord[1] >= shipCenter[1]) {
@@ -65,6 +73,8 @@ canvas.addEventListener('mousemove', (e) => {
       Math.abs(mouseCoord[1] - shipCenter[1]) /
       Math.abs(mouseCoord[0] - shipCenter[0])
     );
+
+    quadrant = 2;
   }
 
   else if (mouseCoord[0] <= shipCenter[0] && mouseCoord[1] >= shipCenter[1]) {
@@ -74,6 +84,8 @@ canvas.addEventListener('mousemove', (e) => {
       Math.abs(mouseCoord[0] - shipCenter[0]) /
       Math.abs(mouseCoord[1] - shipCenter[1])
     );
+
+    quadrant = 3;
   }
 
   else {
@@ -83,11 +95,35 @@ canvas.addEventListener('mousemove', (e) => {
       Math.abs(mouseCoord[1] - shipCenter[1]) /
       Math.abs(mouseCoord[0] - shipCenter[0])
     );
+
+    quadrant = 4;
   }
 
   const mouseAngle = (ratio * 180) / Math.PI;
 
-  ship.rotation = (mouseAngle + degBase).toFixed(0);
+  ship.rotation = parseInt((mouseAngle + degBase).toFixed(0));
+
+  // compute vertex rotation of ship
+  const newVertices = [];
+
+  console.log('new', JSON.stringify(ship));
+
+  ship.vertices.forEach(vertex => {
+    console.log('vert', vertex);
+    const rotationCenter = [0, 0];
+    console.log('rotcent', rotationCenter);
+    const radiusX = Math.abs(vertex[0] - rotationCenter[0]);
+    const radiusY = Math.abs(vertex[1] - rotationCenter[1]);
+    const radius = Math.sqrt((radiusX * radiusX) + (radiusY * radiusY));
+    console.log('rad', radius);
+    const newXVert = parseInt((radius * Math.cos(ship.rotation)).toFixed(0));
+    const newYVert = parseInt((radius * Math.sin(ship.rotation)).toFixed(0));
+    newVertices.push([newXVert, newYVert]);
+  });
+  
+  console.log(newVertices, ship);
+
+  ship.vertices = newVertices;
 
   updateScene();
 
